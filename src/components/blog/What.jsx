334f28cc741a5e8965c86b701f9d7ea30fe4d369
kesolink <kingleyumune14@gmail.com>
2024-02-01 15:14:48 +0100

    new

--- src/components/blog/What.jsx
+++ src/components/blog/What.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "./what.scss"
+import { motion } from "framer-motion"
+import fadeIn from '../../variants';
 import { MdIntegrationInstructions } from "react-icons/md";
 
 export default function What() {
   return (
     <div>
         <div className="what-wrap">
-            <h1 className="hero-text">Are you struggling with?</h1>
-            <div className="box-wrap">
+            <motion.h1 variants={fadeIn('up', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }} className="hero-text">Are you struggling with?</motion.h1>
+            <motion.div variants={fadeIn('left', 0.4)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.7 }}  className="box-wrap">
             <div className="box" >
                 <div className="text-wrap">
                 <span><MdIntegrationInstructions /></span><h1> integration</h1>
@@ -32,7 +34,7 @@ export default function What() {
                 </div>
                 <p>"User can create community in just one button without writing code"</p>
             </div>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
